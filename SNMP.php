2d83da86cdfca10c264e4d11483f7076c35a831d
t.kalapun <t.kalapun@ac5db8e6-f9a5-11dd-a295-0b7b0935bf94>
2009-02-13 11:22:24 +0000

    modified function for converting OID to HEX

--- SNMP.php
+++ SNMP.php
@@ -44,44 +44,49 @@ class SNMP {
      */
     protected static function prepareTrapPacket($varBinds=null, $community='public', $requestId=null) 
 	{     
-        $trap['version']    = self::packVar(1); //pack('H*', '0201'.'01'); // version SNMP v2c
-        $trap['community']  = self::packVar($community);
+        $pSnmp['version']    = self::packVar(1); //pack('H*', '0201'.'01'); // version SNMP v2c
+        $pSnmp['community']  = self::packVar($community);
+		$pSnmp['type']       = pack('H*', 'a7'); // SNMPv2-Trap-PDU
 		
 		$requestId = ($requestId) ? $requestId : rand(10000, 30000); // from 10000 to 40000 ?
-		$trap['header']     = self::packVar($requestId) // requestId
-							. self::packVar(0)			 // errorStatus
-							. self::packVar(0);		 // errorIndex
+		$pSnmp['requestId']  = self::packVar($requestId);
 		
-		$upTime = rand(100, 900) * 1000000;
-        $trap['sysUpTime']  = self::packVarBind(array('oid'   => '1.3.6.1.2.1.1.3.0', 
+		$pSnmp['errorStatus']  = self::packVar(0);
+		$pSnmp['errorIndex']  = self::packVar(0);
+		
+		$upTime = rand(100, 900) * 1000000; //the time in milliseconds
+        $pSnmp['sysUpTime']  = self::packVarBind(array('oid'   => '1.3.6.1.2.1.1.3.0', 
                                                        'value' => $upTime, 
                                                        'type'  => 'o' // x43
                                                       ));
                                                 
-        $trap['snmpTrapOID'] = self::packVarBind(array('oid'   => '1.3.6.1.6.3.1.1.4.1.0', 
+        $pSnmp['snmpTrapOID'] = self::packVarBind(array('oid'   => '1.3.6.1.6.3.1.1.4.1.0', 
                                                        'value' => '1.3.6.1.2.1.31.2.1', 
                                                        'type'  => 'oid'
                                                       ));
         
-        $trap['varBinds']   = $trap['sysUpTime'] 
-						 	. $trap['snmpTrapOID'] 
+        $pSnmp['varBinds']   = $pSnmp['sysUpTime'] 
+						 	. $pSnmp['snmpTrapOID'] 
 						 	. self::packVarBinds($varBinds);
         
-        $trap['varBindsHeader']  = pack('H*', '30' . self::hexlen($trap['varBinds'],1));
+        $pSnmp['varBindsHeader']  = pack('H*', '30' . self::hexlen($pSnmp['varBinds'],1));
         
-        $trap['body'] 		= $trap['header'] 
-							. $trap['varBindsHeader'] 
-							. $trap['varBinds'];
+        $pSnmp['body'] 		= $pSnmp['requestId']
+							. $pSnmp['errorStatus']
+							. $pSnmp['errorIndex']
+							. $pSnmp['varBindsHeader'] 
+							. $pSnmp['varBinds'];
         
-        $trap['head'] = pack('H*', 'a7'. self::hexlen($trap['body'],1));
+        $pSnmp['bodyLen'] = pack('H*', self::hexlen($trap['body'],1));
         
-        $trapPack 	  = $trap['version'] 
-					  . $trap['community'] 
-					  . $trap['head'] 
-					  . $trap['body'];
+        $snmpPack 	  = $pSnmp['version'] 
+					  . $pSnmp['community'] 
+					  . $pSnmp['type']
+					  . $pSnmp['bodyLen'] 
+					  . $pSnmp['body'];
 					
-        $trapPacket   = pack('H*', '30' . self::hexlen($trapPack,1)) 
-					  . $trapPack;
+        $snmpPacket   = pack('H*', '30' . self::hexlen($snmpPack,1)) 
+					  . $snmpPack;
 					
         return $trapPacket;
     }
@@ -112,12 +117,49 @@ class SNMP {
     
     protected static function packVar($var, $type=null, $varInHex=false) 
 	{  
-        /*
-            x04 - string
-            x06 - OID
-            x30 - varBind start
-            x02 - int
-            x43 - object ?
+        /**
+		 * @link http://en.wikipedia.org/wiki/Basic_encoding_rules
+		P/C is the primitive/constructed bit, it specifies if the value is primitive like an INTEGER or constructed which means, it again holds TLV values like a SET. If the bit is "on" (value = 1), it indicates a constructed value.
+		
+		Name 						P/C 	dec	hex
+		EOC (End-of-Content) 		P 		0 	0x00
+		BOOLEAN 					P 		1 	0x01
+		INTEGER 					P 		2 	0x02
+		BIT STRING 					P/C 	3 	0x03
+		OCTET STRING 				P/C 	4 	0x04
+		NULL 						P 		5 	0x05
+		OBJECT IDENTIFIER 			P 		6 	0x06
+		Object Descriptor 			P 		7 	0x07
+		EXTERNAL 					C 		8 	0x08
+		REAL (float) 				P 		9 	0x09
+		ENUMERATED 					P 		10 	0x0A
+		EMBEDDED PDV 				C 		11 	0x0B
+		UTF8String 					P/C 	12 	0x0C
+		RELATIVE-OID 				P 		13 	0x0D
+		SEQUENCE and SEQUENCE OF 	C 		16 	0x10
+		SET and SET OF 				C 		17 	0x11
+		NumericString 				P/C 	18 	0x12
+		PrintableString 			P/C 	19 	0x13
+		T61String 					P/C 	20 	0x14
+		VideotexString 				P/C 	21 	0x15
+		IA5String 					P/C 	22 	0x16
+		UTCTime 					P/C 	23 	0x17
+		GeneralizedTime 			P/C 	24 	0x18
+		GraphicString 				P/C 	25 	0x19
+		VisibleString 				P/C 	26 	0x1A
+		GeneralString 				P/C 	27 	0x1B
+		UniversalString 			P/C 	28 	0x1C
+		CHARACTER STRING 			P/C 	29 	0x1D
+		BMPString 					P/C 	30 	0x1E
+            
+		IpAddress						40
+		Counter (Counter32 in SNMPv2)	41
+		Gauge (Gauge32 in SNMPv 2)		42
+		TimeTicks						43
+		Opaque							44
+		NsapAddress						45
+		Counter64 (available only in SNMPv2)	46
+		Uinteger32 (available only in SNMPv2)	47
         */
         $type2hex   = array( 's'=>'04', 'i'=>'02', 'oid'=>'06', 'o'=>'43');
         
@@ -131,44 +173,16 @@ class SNMP {
         }
 
         $varHex = '';
-        if ($type == 'i')   $varHex = ($varInHex) ? $var : self::dec2hex($var); 
-        if ($type == 's')   $varHex = ($varInHex) ? $var : self::str2hex($var);
-        if ($type == 'o')   $varHex = ($varInHex) ? $var : self::dec2hex($var);
+        if ($type == 'i')   $varHex = ($varInHex) ? $var : dec2hex($var); 
+        if ($type == 's')   $varHex = ($varInHex) ? $var : str2hex($var);
+        if ($type == 'o')   $varHex = ($varInHex) ? $var : dec2hex($var);
         if ($type == 'x')   $varHex = ($varInHex) ? $var : $var; // if object, we pass hex data in $var
-        if ($type == 'oid') $varHex = ($varInHex) ? $var : self::oid2hex($var);
+        if ($type == 'oid') $varHex = ($varInHex) ? $var : oid2hex($var);
         
         $bodyPack = pack('H*', $varHex);
         $headPack = pack('H*', $typeHex . self::hexlen($bodyPack) );
         
         return $headPack . $bodyPack;
-    }
-    
-    protected static function str2hex($s) 
-	{
-        $hex = '';
-        for ($i=0;$i<strlen($s);$i++) $hex .= self::dec2hex(ord($s[$i]));
-        return $hex;
-    }
-    
-    protected static function dec2hex($i) 
-	{
-        $i = dechex($i);
-        if (strlen($i) & 1 == 1) $i = '0'.$i;
-        return $i;
-    }
-    
-    protected static function oid2hex($oid) 
-	{
-        $oidHex = '';
-        if (substr($oid, 0, 3) == '1.3') {
-            $oidHex .= '2b';
-            $oid = substr($oid, 3);
-        }
-        $oid = trim($oid, '.');
-        $oid = explode('.', $oid);
-        foreach ($oid as $n) $oidHex .= self::dec2hex($n);
-
-        return $oidHex;
     }
     
 	protected static function hexlen($s, $showPrefix=false) 
@@ -178,7 +192,36 @@ class SNMP {
         if ($len > 255)   $prefix = '82';
         if ($len > 65535) $prefix = '83';
         
-        return $prefix . self::dec2hex($len);
+        return $prefix . dec2hex($len);
     }
 
 }
+
+	function str2hex($s) 
+	{
+        $hex = '';
+        for ($i=0;$i<strlen($s);$i++) $hex .= dec2hex(ord($s[$i]));
+        return $hex;
+    }
+	
+	function oid2hex($n) 
+	{
+		if (is_int($n)) {
+			$oidHex = ($n>127) ? dec2hex(128|$n/128) . dec2hex($n%128) : dec2hex($n);
+		} else {
+			$oidHex = '';
+			$oid = explode('.', $n);
+			$n1 = (40 * $oid[0]) + $oid[1];
+			$oidHex .= dec2hex($n1);
+			unset($oid[0], $oid[1]);
+			foreach ($oid as $n) $oidHex .= oid2hex( (int)$n );
+		}
+        return $oidHex;
+    }
+	
+	function dec2hex($n) 
+	{
+        $n = dechex($n);
+        if (strlen($n) & 1 == 1) $n = '0'.$n;
+        return $n;
+    }
