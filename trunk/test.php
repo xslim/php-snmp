bedd5f48eed863c788b96e07abe14dd2d28a4ba2
t.kalapun <t.kalapun@ac5db8e6-f9a5-11dd-a295-0b7b0935bf94>
2009-02-13 11:27:32 +0000

    added some test script

---
+++ trunk/test.php
@@ -0,0 +1,49 @@
+<?
+
+//echo base_convert(2680, 10, 128);
+echo oid2hex('1.3.1981');
+echo "<br>";
+echo 128 | 2680/128;
+
+function getNumericStringIndex($string_idx) {
+     $string_idx = substr($string_idx, 1, strlen($string_idx)-2);
+     $ret = strlen($string_idx);
+     for ($i=0; $i<strlen($string_idx); $i++)
+        $ret .= '.'.ord($string_idx[$i]);
+
+     return $ret;
+}
+
+	function oid2hex($n) 
+	{
+		if (is_int($n)) {
+			$oidHex = ($n>127) ? dec2hex(128|$n/128) . dec2hex($n%128) : dec2hex($n);
+		} else {
+			$oidHex = '';
+			$oid = explode('.', $n);
+			$n1 = (40 * $oid[0]) + $oid[1];
+			$oidHex .= dec2hex($n1);
+			unset($oid[0], $oid[1]);
+			foreach ($oid as $n) $oidHex .= oid2hex( (int)$n );
+		}
+        return $oidHex;
+    }
+	
+	function dec2hex($n) 
+	{
+        $n = dechex($n);
+        if (strlen($n) & 1 == 1) $n = '0'.$n;
+        return $n;
+    }
+
+/*
+	
+static void MakeBase128( unsigned long l, int first ){
+	if( l > 127 )	{		MakeBase128( l / 128, 0 );	}
+	l %= 128;
+	if( first )	{		abBinary[nBinary++] = (unsigned char)l;	}
+	else	{		abBinary[nBinary++] = 0x80 | (unsigned char)l;	}
+}
+*/
+	
+?>
